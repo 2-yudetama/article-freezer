382883a9c7b7f9c2578a1a33b2482f4138884419
2-yudetama <253080096+2-yudetama@users.noreply.github.com>
2026-02-16 23:19:26 +0900

    🛂 自前管理のユーザ情報を用いたjwtセッション管理を実装 (#12)

--- packages/web-app/src/lib/auth/auth.ts
+++ packages/web-app/src/lib/auth/auth.ts
@@ -1,3 +1,4 @@
+import { prisma } from "db";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
@@ -8,6 +9,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     maxAge: 86400, // 1 day
   },
   callbacks: {
+    /**
+     * Authorizedコールバック
+     * @see https://authjs.dev/reference/nextjs#authorized
+     */
     authorized: ({ request, auth }) => {
       // URLからオリジンとパスを取得
       const origin = request.nextUrl.origin;
@@ -29,6 +34,100 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
 
       return true;
+    },
+    /**
+     * SignInコールバック
+     * @see https://authjs.dev/reference/nextjs#signin
+     */
+    signIn: async ({ user, account }) => {
+      if (!user || !account) {
+        console.error("[Auth] User or Account is missing");
+        return false;
+      }
+
+      try {
+        // ユーザ情報をDBにupsert
+        const dbUser = await prisma.user.upsert({
+          where: {
+            provider_provider_account_id: {
+              provider: account.provider,
+              provider_account_id: account.providerAccountId,
+            },
+          },
+          create: {
+            provider: account.provider,
+            provider_account_id: account.providerAccountId,
+            name: user.name ?? "unknown",
+            email: user.email ?? "unknown",
+            image: user.image ?? "unknown",
+          },
+          update: {
+            name: user.name ?? "unknown",
+            email: user.email ?? "unknown",
+            image: user.image ?? "unknown",
+          },
+        });
+
+        // 必要な情報を代入
+        user.id = dbUser.user_id;
+        user.provider = dbUser.provider;
+        user.provider_account_id = dbUser.provider_account_id;
+        user.role = dbUser.role;
+
+        console.log("[Auth] Upsert user successfully: ", dbUser);
+        return true;
+      } catch (error) {
+        // DBエラーは認証エラーとする
+        console.error("[Auth] DB upsert error: ", error);
+        return false;
+      }
+    },
+    /**
+     * JWTコールバック
+     * @see https://authjs.dev/reference/nextjs#jwt
+     */
+    jwt: ({ token, user, account }) => {
+      // サインイン直後はuserとaccountを持つ
+      if (user && account) {
+        // jwt作成
+        return {
+          ...token,
+          id: user.id,
+          sub: user.id,
+          provider: user.provider,
+          provider_account_id: user.provider_account_id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+          role: user.role,
+        };
+      }
+
+      /**
+       * NOTE: jwtコールバックがアクセスされるたびにセッションの期限が更新される
+       * @see https://github.com/nextauthjs/next-auth/issues/13248
+       */
+      return token;
+    },
+    /**
+     * Sessionコールバック
+     * @see https://authjs.dev/reference/nextjs#session
+     */
+    session: ({ session, token }) => {
+      // tokenの情報で更新して返す
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          provider: token.provider,
+          provider_account_id: token.provider_account_id,
+          name: token.name,
+          email: token.email,
+          image: token.image,
+          role: token.role,
+        },
+      };
     },
   },
 });
